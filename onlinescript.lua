aa71175b56cd832f31a56613105b83447999b0b4
chanahack <163324307+chanahack@users.noreply.github.com>
2024-05-02 18:25:51 +0700

    Add files via upload

---
+++ onlinescript.lua
@@ -0,0 +1,8 @@
+
+API = gg.makeRequest('https://github.com/chanahack/Winhack/raw/main/ScriptPro.lua').content
+if not API then
+gg.alert('⚠️คุณออฟไลน์อยู่⚠️\nOR\n⚠⚠คุณไม่ได้ให้การเข้าถึงอินเทอร์เน็ต⚠⚠')
+noselect()
+else
+pcall(load(API))
+end
