16b08d65aaf89b74264a06b41346960b07687161
chanahack <163324307+chanahack@users.noreply.github.com>
2024-12-01 21:31:44 +0700

    onlinescript.lua

--- onlinescript.lua
+++ onlinescript.lua
@@ -1,5 +1,5 @@
 
-API = gg.makeRequest('https://github.com/chanahack/HEEYai/raw/refs/heads/main/ScriptPro.lua').content
+API = gg.makeRequest('https://github.com/chanahack/HEEYai/raw/refs/heads/main/ScriptnormalPRO.lua').content
 if not API then
 gg.alert('⚠️>คุณออฟไลน์อยู่<⚠️\nOR\n⚠⚠คุณไม่ได้ให้การเข้าถึงอินเทอร์เน็ต⚠⚠')
 noselect()
